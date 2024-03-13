89fc7730d8a572ac89ace851829162e35eefacaf
Nimrajarral <nimrajarral@gmail.com>
2024-03-14 01:56:20 +0500

    first commit

---
+++ task7.js
@@ -0,0 +1,11 @@
+// Day 3 Challange:Start Coding//   Question No 7 
+//Number Eight:Write Addition Multliplication,division,subtraction operation thateach result
+//in the number eight .Enclose your operation in print statements. 
+//  addition operation //
+console.log(4 + 4);
+//  multliplication operation //
+console.log(4 * 2);
+//  subtraction operation //
+console.log(16 - 8);
+//  division operation //
+console.log(16 / 2);
